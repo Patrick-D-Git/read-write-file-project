dc35fadc885db8c5a6cf0f41ed956ecdfc474168
Patrick D <patrick.montejo@gmail.com>
2024-02-29 21:41:36 -0500

    updated code

--- main.py
+++ main.py
@@ -4,22 +4,17 @@
 with open("./Input/Names/Invited_names.txt", "r") as name_file:
     invited_names = name_file.readlines()
 
-invited_names_list = []
-for name in invited_names:
-    new_name = name.strip()
-    invited_names_list.append(new_name)
-
-
 with open("./Input/Letters/starting_letter.txt", "r") as letter_file:
     starting_letter_content = letter_file.read()
 
-for name in invited_names_list:
+for name in invited_names:
 
     # Replace the [name] placeholder with the actual name.
-    invitation_letter = starting_letter_content.replace("[name]", name)
+    new_name = name.strip()
+    invitation_letter = starting_letter_content.replace("[name]", new_name)
 
     # Save the letters in the folder "ReadyToSend".
-    with open(f"./Output/ReadyToSend/letter_to_{name}.txt", "w") as invitation_file:
+    with open(f"./Output/ReadyToSend/letter_to_{new_name}.txt", "w") as invitation_file:
         invitation_file.write(invitation_letter)
 
 
